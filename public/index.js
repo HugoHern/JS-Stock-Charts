ac33950cd3333c7e23a261b56af0b9d9780451b8
HugoHern <hugo8954@gmail.com>
2022-04-07 19:23:09 -0400

    added charts

--- public/index.js
+++ public/index.js
@@ -4,6 +4,40 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    //let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=695d8a8ab1a246a086a8a6228b16e48a')
+    
+   
+    //let result = await response.json()
+
+    const { GME, MSFT, DIS, BNTX } = mockData
+
+    const stocks = [GME, MSFT, DIS, BNTX]
+
+    //console.log(result)
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                
+                ],
+                
+            }]
+        },
+       
+    });
+    
 }
 
 main()
+
+console.log(Chart)
